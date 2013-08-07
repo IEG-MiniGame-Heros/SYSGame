4a43a0792a1b45d498239416e51a8551c1e8e8ce
unknown <klc1630@126.com>
2013-08-07 17:52:42 +0800

    提交登录场景...

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -2,6 +2,7 @@
 #include "CCEGLView.h"
 #include "AppDelegate.h"
 #include "FirstStage.h"
+#include "LoginScrene.h"
 #include "SimpleAudioEngine.h"
 
 using namespace CocosDenshion;
@@ -40,8 +41,8 @@ bool AppDelegate::applicationDidFinishLaunching()
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    //CCScene *pScene = HelloWorld::scene();
-	CCScene* pScene = FirstStage::scene();
+    CCScene *pScene = LoginScrene::scene();
+	//CCScene* pScene = FirstStage::scene();
 
     // run
     pDirector->runWithScene(pScene);
---
+++ Classes/LoginScrene.cpp
@@ -0,0 +1,41 @@
+#include "LoginScrene.h"
+
+bool LoginScrene::init()
+{
+	bool bRef = true;
+	do{
+		CC_BREAK_IF(! CCLayer::init());
+		ul = UILayer::create();
+		// 设置 UI 层的tag
+		this->addChild(ul, 0, 100);
+		ul->addWidget(CCUIHELPER->createWidgetFromJsonFile("ui/login/LoginUI_1.ExportJson"));
+
+		// 用户名输入框
+		tfName = dynamic_cast<UITextField*>(ul->getWidgetByName("TextField_username"));
+
+		// 密码输入框
+		tfPsw = dynamic_cast<UITextField*>(ul->getWidgetByName("TextField_password"));
+
+		// 登录按钮
+		tbLogin = dynamic_cast<UITextButton*>(ul->getWidgetByName("Btn_login"));
+		tbLogin->addReleaseEvent(this, coco_releaseselector(LoginScrene::tbLoginCallback));
+		tbLogin->setTextColor(0, 0, 0);
+
+		ul->setTouchEnabled(true);
+		bRef = true;
+	}while(0);
+	return bRef;
+}
+
+CCScene* LoginScrene::scene()
+{
+	CCScene *scene = CCScene::create();
+	CCLayer *layer = LoginScrene::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+void LoginScrene::tbLoginCallback(cocos2d::CCObject *pSender)
+{
+
+}
