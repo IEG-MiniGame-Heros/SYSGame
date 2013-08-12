a6e4ffdf85c67e4723e3517b4bb56d14896157b0
aeronzhou <aeronzhou@qq.com>
2013-08-12 22:46:49 +0800

    增加GameHelper

---
+++ Classes/GameHelper.cpp
@@ -0,0 +1,22 @@
+#include "GameHelper.h"
+
+void GameHelper::onEnter()
+{
+	CCNode::onEnter();
+
+}
+
+void GameHelper::onExit()
+{
+
+}
+
+void GameHelper::onUpdateMonster(float dt)
+{
+
+}
+
+void GameHelper::onUpdateGoods(float dt)
+{
+
+}
---
+++ Classes/GameHelper.h
@@ -0,0 +1,23 @@
+#ifndef SYS_GAME_HELPER
+#define SYS_GAME_HELPER
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+/** 
+ * 这个类作为刷怪、刷道具的一个辅助类
+ */
+class GameHelper : public CCNode
+{
+public:
+
+	void onEnter();
+	void onExit();
+
+	void onUpdateMonster(float dt);
+	void onUpdateGoods(float dt);
+
+};
+
+#endif
